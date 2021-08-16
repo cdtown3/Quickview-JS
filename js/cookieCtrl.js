f06279014902a3bb12e47bbfa4e3cc4766e04dd9
Drew <cdtown3@gmail.com>
2021-08-16 17:04:42 -0400

    Adding comments

--- js/cookieCtrl.js
+++ js/cookieCtrl.js
@@ -1,15 +1,17 @@
 // Angular controller that now contains all cookies and images in an array of dictionaries.
 angular.module('myApp', []).controller('cookieCtrl', function ($scope) {
-    $scope.cookieRowOne = [
-        { name: 'Oatmeal Raisin', img: '/images/OatRaisW.png' },
-        { name: 'Oatmeal & Cinnamon Chip', img: '/images/OatCinW.png' },
-        { name: 'Oatmeal & Butterscotch', img: '/images/OatButterW.png' },
-        { name: 'Lemon with White Chocolate Chip', img: '/images/LemWChocW.png' },
-        { name: 'White Chocolate Macadamia Nut', img: '/images/WChocMacNutW.png' },
-        { name: 'Sugar Cookie', img: '/images/SugarW.png' },
-        { name: 'Snickerdoodle', img: '/images/SnickerW.png' },
-        { name: 'Peanut Butter Chocolate Chip', img: '/images/PeanutChocW.png' },
-        { name: 'Chocolate Chip', img: '/images/ChocW.png'}
+    // Array of dictionaries containing cookie info
+    $scope.cookies = [
+        { 'Name': 'Oatmeal Raisin', 'Img': '/images/OatRaisW.png' },
+        { 'Name': 'Oatmeal & Cinnamon Chip', 'Img': '/images/OatCinW.png' },
+        { 'Name': 'Oatmeal & Butterscotch', 'Img': '/images/OatButterW.png' },
+        { 'Name': 'Lemon with White Chocolate Chip', 'Img': '/images/LemWChocW.png' },
+        { 'Name': 'White Chocolate Macadamia Nut', 'Img': '/images/WChocMacNutW.png' },
+        { 'Name': 'Sugar Cookie', 'Img': '/images/SugarW.png' },
+        { 'Name': 'Snickerdoodle', 'Img': '/images/SnickerW.png' },
+        { 'Name': 'Peanut Butter Chocolate Chip', 'Img': '/images/PeanutChocW.png' },
+        { 'Name': 'Chocolate Chip', 'Img': '/images/ChocW.png' },
     ];
+    // setting search variable to nothing to start
     $scope.search = "";
 });
